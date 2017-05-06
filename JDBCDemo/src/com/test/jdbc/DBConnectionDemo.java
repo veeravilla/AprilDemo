b9430db28399c2f6315c4489a6c7e7eeb39e5ccb
Veeru <Veeru@Veeru-PC>
2017-05-06 10:22:58 -0400

    Jdbc Demo

---
+++ JDBCDemo/src/com/test/jdbc/DBConnectionDemo.java
@@ -0,0 +1,25 @@
+package com.test.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectionDemo {
+
+	public static void main(String[] args) throws Exception{
+		
+		//Load DB Driver
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//Get Connection
+       // URL(host name,port,dbname) / username / password
+		Connection connection = DriverManager.
+								getConnection("jdbc:mysql://localhost:3306/april2017","root","root");
+	
+		//Do JDBC Operations
+		System.out.println(connection);
+	
+		//close connection
+		connection.close();
+	}
+
+}
